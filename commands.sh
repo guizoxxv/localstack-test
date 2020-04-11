2d2b166f5ac96165a577f86a535181203ac78638
guizoxxv <guizoxxv@gmail.com>
2020-04-10 23:13:24 -0300

    Initial commit

---
+++ commands.sh
@@ -0,0 +1,36 @@
+# Create bucket
+aws --endpoint-url=http://127.0.0.1:4572 s3api create-bucket --bucket mybucket
+
+# List buckets
+aws --endpoint-url=http://127.0.0.1:4572 s3api list-buckets
+
+# Add ACL (Acess Control List) rules to bucket
+aws --endpoint-url=http://127.0.0.1:4572 s3api put-bucket-acl --bucket mybucket --acl public-read
+
+# Configure notification on bucket
+aws --endpoint-url=http://127.0.0.1:4572 s3api put-bucket-notification-configuration --bucket mybucket --notification-configuration file://notification.json
+
+# Create queue
+aws --endpoint-url=http://127.0.0.1:4576 sqs create-queue --queue-name myqueue
+
+# List queues
+aws --endpoint-url=http://127.0.0.1:4576 sqs list-queues
+
+# Get queue attributes and query ARN (Amazon Resource Name) value
+aws --endpoint-url=http://127.0.0.1:4576 sqs get-queue-attributes --queue-url http://localhost:4576/queue/myqueue --attribute-names All --output text --query 'Attributes.QueueArn'
+
+QUEUE_ARN=$(aws --endpoint-url=http://127.0.0.1:4576 sqs get-queue-attributes --queue-url http://localhost:4576/queue/myqueue --attribute-names All --output text --query 'Attributes.QueueArn')
+
+sed -i "s/{{queueArn}}/$QUEUE_ARN/g" notification.json
+
+# Copy local object to bucket
+aws --endpoint-url=http://127.0.0.1:4572 s3 cp sample.jpg s3://mybucket/
+
+# List bucket files
+aws --endpoint-url=http://127.0.0.1:4572 s3 ls s3://mybucket
+
+# Get queue messages
+aws --endpoint-url=http://127.0.0.1:4576 sqs receive-message --queue-url http://localhost:4576/queue/myqueue --attribute-names All --message-attribute-names All --max-number-of-messages 10
+
+# Sync local files to bucket
+aws --endpoint-url=http://127.0.0.1:4572 s3 sync /root/localstack/s3 s3://mybucket/
---
+++ localstack/s3/config/config.sh
@@ -0,0 +1,20 @@
+# Create bucket
+aws --endpoint-url=http://127.0.0.1:4572 s3api create-bucket --bucket mybucket
+
+# Add ACL (Acess Control List) rules to bucket
+aws --endpoint-url=http://127.0.0.1:4572 s3api put-bucket-acl --bucket mybucket --acl public-read
+
+# Create queue
+aws --endpoint-url=http://127.0.0.1:4576 sqs create-queue --queue-name myqueue
+
+# Get QueueArn attribute and store in QUEUE_ARN variable
+QUEUE_ARN=$(aws --endpoint-url=http://127.0.0.1:4576 sqs get-queue-attributes --queue-url http://localhost:4576/queue/myqueue --attribute-names All --output text --query 'Attributes.QueueArn')
+
+# Replace {{queueuArn}} by QUEUE_ARN variable value in notifications configuration JSON file
+sed -i "s/{{queueArn}}/$QUEUE_ARN/g" notification.json
+
+# Configure notification on bucket
+aws --endpoint-url=http://127.0.0.1:4572 s3api put-bucket-notification-configuration --bucket mybucket --notification-configuration file://notification.json
+
+# Sync local files to bucket
+aws --endpoint-url=http://127.0.0.1:4572 s3 sync /root/localstack/s3 s3://mybucket/
